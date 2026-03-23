bf9ed5a07e92988386ed7d5e7b4c52e3dd0d7d64
satalite30 <sergioudea91>
2026-03-23 15:01:06 -0500

    prepare push to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+export default defineConfig({
+  site: 'https://TU-USUARIO.github.io',
+  base: '/NOMBRE-DEL-REPO',
+  output: 'static',
+  adapter: github(),
+});
+
 // https://astro.build/config
 // Modo 'server' para permitir SSR en rutas dinámicas como [slug].astro
 // Alternativamente, podrías usar 'static' (defecto) pero requeriría reconstruir al agregar posts
-export default defineConfig({
+/* export default defineConfig({
   output: 'server'
-});
+}); */
